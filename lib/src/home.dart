d80991b4828c42b395bea433f56ff8f32a8d7909
Besslan <besslan.bazadough@gmail.com>
2019-01-19 12:57:50 +0200

    add bottom navigation bar , refactor navigation

--- lib/src/home.dart
+++ lib/src/home.dart
@@ -1,19 +1,11 @@
 import 'package:flutter/material.dart';
-import 'drawer.dart';
-import 'body.dart';
-
+// import 'drawer.dart';
+// import 'body.dart';
+import 'navigation.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        backgroundColor: Colors.pinkAccent[700],
-        title: Text('PrepApp'),
-      ),
-      drawer: DrawerNavigation(),
-      body: Body()
-    );
+    return Navigation();
   }
 }
---
+++ lib/src/navigation.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'drawer.dart';
+
+class Navigation extends StatefulWidget {
+ Navigation({Key key}) : super(key: key);
+
+ @override
+ NavigationState createState() => NavigationState();
+}
+
+ class NavigationState extends State<Navigation> {
+ int _selectedIndex = 1;
+ final _widgetOptions = [
+   Text('Index 0: Home'),
+   Text('Index 1: Login'),
+   Text('Index 2: Profile'),
+ ];
+
+ @override
+ Widget build(BuildContext context) {
+   return Scaffold(
+    appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: Colors.pinkAccent[700],
+        title: Text('PrepApp'),
+      ),
+     body: Center(
+       child: _widgetOptions.elementAt(_selectedIndex),
+     ),
+      drawer: DrawerNavigation(),
+     bottomNavigationBar: BottomNavigationBar(
+       items: <BottomNavigationBarItem>[
+         BottomNavigationBarItem(icon: Icon(Icons.home), title: Text('Home')),
+         BottomNavigationBarItem(icon: Icon(Icons.vpn_key), title: Text('Login')),
+         BottomNavigationBarItem(icon: Icon(Icons.account_box), title: Text('Profile')),
+       ],
+       currentIndex: _selectedIndex,
+       fixedColor: Colors.deepPurple,
+       onTap: _onItemTapped,
+     ),
+   );
+ }
+
+ void _onItemTapped(int index) {
+   setState(() {
+     _selectedIndex = index;
+   });
+ }
+}
