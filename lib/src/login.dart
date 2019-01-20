decac6b304db7124a27e6e4b71f64c0d3dbee068
Besslan <besslan.bazadough@gmail.com>
2019-01-20 18:36:44 +0200

    create login screen and modify nagigation to render the login page

---
+++ lib/src/login.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  static String tag = 'login-page';
+  @override
+  LoginPageState createState() => new LoginPageState();
+}
+
+class LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: ListView(
+          shrinkWrap: true,
+          padding: EdgeInsets.only(left: 24.0, right: 24.0),
+          children: <Widget>[
+            Hero(
+              tag: 'hero',
+              child: CircleAvatar(
+                backgroundColor: Colors.transparent,
+                radius: 48.0,
+                child: Image.asset('assets/small-Logo.png'),
+              ),
+            ),
+            SizedBox(height: 48.0),
+            TextFormField(
+              keyboardType: TextInputType.emailAddress,
+              // autofocus: false,
+              decoration: InputDecoration(
+                hintText: 'Email',
+                contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+                border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10.0)),
+              ),
+            ),
+            SizedBox(height: 8.0),
+            TextFormField(
+              autofocus: false,
+              obscureText: true,
+              decoration: InputDecoration(
+                hintText: 'Password',
+                contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+                border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10.0)),
+              ),
+            ),
+            SizedBox(height: 24.0),
+            Padding(
+              padding: EdgeInsets.symmetric(vertical: 16.0),
+              child: RaisedButton(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                onPressed: () {},
+                padding: EdgeInsets.all(12),
+                color: Colors.pinkAccent[700],
+                child: Text('Log In', style: TextStyle(color: Colors.white)),
+              ),
+            ),
+            //         FlatButton(
+            //   child: Text(
+            //     'Forgot password?',
+            //     style: TextStyle(color: Colors.black54),
+            //   ),
+            //   onPressed: () {},
+            // )
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/src/navigation.dart
+++ lib/src/navigation.dart
@@ -1,50 +1,53 @@
 import 'package:flutter/material.dart';
 import 'drawer.dart';
 import 'homePageBody.dart';
-
+import 'login.dart';
+import 'profile.dart';
 class Navigation extends StatefulWidget {
- Navigation({Key key}) : super(key: key);
+  Navigation({Key key}) : super(key: key);
 
- @override
- NavigationState createState() => NavigationState();
+  @override
+  NavigationState createState() => NavigationState();
 }
 
- class NavigationState extends State<Navigation> {
- int _selectedIndex = 0;
- final _widgetOptions = [
-   HomePageBody(),
-   Text('Index 1: Login'),
-   Text('Index 2: Profile'),
- ];
+class NavigationState extends State<Navigation> {
+  int _selectedIndex = 0;
+  final _widgetOptions = [
+    HomePageBody(),
+    LoginPage(),
+    Profile(),
+  ];
 
- @override
- Widget build(BuildContext context) {
-   return Scaffold(
-    appBar: AppBar(
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
         centerTitle: true,
         backgroundColor: Colors.pinkAccent[700],
         title: Text('PrepApp'),
       ),
-     body: Center(
-       child: _widgetOptions.elementAt(_selectedIndex),
-     ),
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
       drawer: DrawerNavigation(),
-     bottomNavigationBar: BottomNavigationBar(
-       items: <BottomNavigationBarItem>[
-         BottomNavigationBarItem(icon: Icon(Icons.home), title: Text('Home')),
-         BottomNavigationBarItem(icon: Icon(Icons.vpn_key), title: Text('Login')),
-         BottomNavigationBarItem(icon: Icon(Icons.account_box), title: Text('Profile')),
-       ],
-       currentIndex: _selectedIndex,
-       fixedColor: Colors.deepPurple, 
-       onTap: _onItemTapped,
-     ),
-   );
- }
+      bottomNavigationBar: BottomNavigationBar(
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(icon: Icon(Icons.home), title: Text('Home')),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.vpn_key), title: Text('Login')),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.account_box), title: Text('Profile')),
+        ],
+        currentIndex: _selectedIndex,
+        fixedColor: Colors.deepPurple,
+        onTap: _onItemTapped,
+      ),
+    );
+  }
 
- void _onItemTapped(int index) {
-   setState(() {
-     _selectedIndex = index;
-   });
- }
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
 }
