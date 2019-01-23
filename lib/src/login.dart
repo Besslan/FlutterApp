c636581c648f1b731022d13839a6177c3d4371e2
Besslan <besslan.bazadough@gmail.com>
2019-01-23 13:11:02 +0200

    refactor login to use From, validators

--- lib/src/login.dart
+++ lib/src/login.dart
@@ -8,97 +8,116 @@ class LoginPage extends StatefulWidget {
 }
 
 class LoginPageState extends State<LoginPage> {
-  bool loggedin = false;
+  final formKey = GlobalKey<FormState>();
+
   String email;
-  String passwprd;
+  String password;
 
   TextEditingController emailController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    if (loggedin == false) {
-      return Center(
-        child: ListView(
-          shrinkWrap: true,
-          padding: EdgeInsets.only(left: 24.0, right: 24.0),
-          children: <Widget>[
-            Hero(
-              tag: 'hero',
-              child: CircleAvatar(
-                backgroundColor: Colors.transparent,
-                radius: 48.0,
-                child: Image.asset('assets/small-Logo.png'),
-              ),
+    return Center(
+      child: ListView(
+        shrinkWrap: true,
+        padding: EdgeInsets.only(left: 24.0, right: 24.0),
+        children: <Widget>[
+          Hero(
+            tag: 'hero',
+            child: CircleAvatar(
+              backgroundColor: Colors.transparent,
+              radius: 48.0,
+              child: Image.asset('assets/small-Logo.png'),
             ),
-            SizedBox(height: 48.0),
-            TextFormField(
-              controller: emailController,
-              keyboardType: TextInputType.emailAddress,
-              // autofocus: false,
-              decoration: InputDecoration(
-                hintText: 'Email',
-                contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
-                border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10.0)),
-              ),
+          ),
+          Form(
+            key: formKey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                emailFeild(),
+                passwordFeild(),
+                submitButton(),
+              ],
             ),
-            SizedBox(height: 8.0),
-            TextFormField(
-              controller: passwordController,
-              autofocus: false,
-              obscureText: true,
-              decoration: InputDecoration(
-                hintText: 'Password',
-                contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
-                border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10.0)),
-              ),
-            ),
-            SizedBox(height: 24.0),
-            Padding(
-              padding: EdgeInsets.symmetric(vertical: 16.0),
-              child: RaisedButton(
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                onPressed: () {
-                  setState(() {
-                    email = emailController.text;
-                    passwprd = passwordController.text;
-                  });
-                  signIn();
-                },
-                padding: EdgeInsets.all(12),
-                color: Colors.pinkAccent[700],
-                child: Text('Log In', style: TextStyle(color: Colors.white)),
-              ),
-            ),
-            //         FlatButton(
-            //   child: Text(
-            //     'Forgot password?',
-            //     style: TextStyle(color: Colors.black54),
-            //   ),
-            //   onPressed: () {},
-            // )
-          ],
-        ),
-      );
-    } else {
-      return Text('already logged in');
-    }
+          ),
+        ],
+      ),
+    );
   }
 
   Future<void> signIn() async {
     try {
       FirebaseUser user = await FirebaseAuth.instance
-          .signInWithEmailAndPassword(email: email, password: passwprd);
+          .signInWithEmailAndPassword(email: email, password: password);
       Navigator.push(
         context,
-        MaterialPageRoute(builder: (context) => Profile(user:user)),
+        MaterialPageRoute(builder: (context) => Profile(user: user)),
       );
     } catch (err) {
       print(err.message);
     }
+  }
+
+  Widget emailFeild() {
+    return TextFormField(
+      keyboardType: TextInputType.emailAddress,
+      decoration: InputDecoration(
+        labelText: 'Email Adress',
+        hintText: 'you@example.com',
+      ),
+      validator: (String value) {
+        if (!value.contains('@')) {
+          return 'Enter a valie email';
+        }
+      },
+      onSaved: (String value) {
+        setState(() {
+          email = value;
+        });
+      },
+    );
+  }
+
+  Widget passwordFeild() {
+    return TextFormField(
+      keyboardType: TextInputType.emailAddress,
+      decoration: InputDecoration(
+        labelText: 'Password',
+        hintText: 'Password',
+      ),
+      obscureText: true,
+      validator: (String value) {
+        if (value.length < 4) {
+          return 'Password must be at least 4 letters long';
+        }
+      },
+      onSaved: (String value) {
+        setState(() {
+          password = value;
+        });
+      },
+    );
+  }
+
+  Widget submitButton() {
+    return Padding(
+      padding: EdgeInsets.symmetric(vertical: 16.0),
+      child: RaisedButton(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10),
+        ),
+        onPressed: () {
+          if (formKey.currentState.validate()) {
+            formKey.currentState.save();
+            signIn();
+          }
+        },
+        padding: EdgeInsets.all(12),
+        color: Colors.pinkAccent[700],
+        child: Text('Submit', style: TextStyle(color: Colors.white)),
+      ),
+    );
   }
 }
