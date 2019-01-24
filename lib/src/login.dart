0c58953237cce9df3d9eecdbfda82a01df2d6d84
Besslan <besslan.bazadough@gmail.com>
2019-01-24 13:57:58 +0200

    comment unused code for future refence

--- lib/src/login.dart
+++ lib/src/login.dart
@@ -1,123 +1,123 @@
-import 'package:flutter/material.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'profile.dart';
+// import 'package:flutter/material.dart';
+// import 'package:firebase_auth/firebase_auth.dart';
+// import 'profile.dart';
 
-class LoginPage extends StatefulWidget {
-  @override
-  LoginPageState createState() => new LoginPageState();
-}
+// class LoginPage extends StatefulWidget {
+//   @override
+//   LoginPageState createState() => new LoginPageState();
+// }
 
-class LoginPageState extends State<LoginPage> {
-  final formKey = GlobalKey<FormState>();
+// class LoginPageState extends State<LoginPage> {
+//   final formKey = GlobalKey<FormState>();
 
-  String email;
-  String password;
+//   String email;
+//   String password;
 
-  TextEditingController emailController = TextEditingController();
-  TextEditingController passwordController = TextEditingController();
+//   TextEditingController emailController = TextEditingController();
+//   TextEditingController passwordController = TextEditingController();
 
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: ListView(
-        shrinkWrap: true,
-        padding: EdgeInsets.only(left: 24.0, right: 24.0),
-        children: <Widget>[
-          Hero(
-            tag: 'hero',
-            child: CircleAvatar(
-              backgroundColor: Colors.transparent,
-              radius: 48.0,
-              child: Image.asset('assets/small-Logo.png'),
-            ),
-          ),
-          Form(
-            key: formKey,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: <Widget>[
-                emailFeild(),
-                passwordFeild(),
-                submitButton(),
-              ],
-            ),
-          ),
-        ],
-      ),
-    );
-  }
+//   @override
+//   Widget build(BuildContext context) {
+//     return Center(
+//       child: ListView(
+//         shrinkWrap: true,
+//         padding: EdgeInsets.only(left: 24.0, right: 24.0),
+//         children: <Widget>[
+//           Hero(
+//             tag: 'hero',
+//             child: CircleAvatar(
+//               backgroundColor: Colors.transparent,
+//               radius: 48.0,
+//               child: Image.asset('assets/small-Logo.png'),
+//             ),
+//           ),
+//           Form(
+//             key: formKey,
+//             child: Column(
+//               crossAxisAlignment: CrossAxisAlignment.stretch,
+//               children: <Widget>[
+//                 emailFeild(),
+//                 passwordFeild(),
+//                 submitButton(),
+//               ],
+//             ),
+//           ),
+//         ],
+//       ),
+//     );
+//   }
 
-  Future<void> signIn() async {
-    try {
-      FirebaseUser user = await FirebaseAuth.instance
-          .signInWithEmailAndPassword(email: email, password: password);
-      Navigator.push(
-        context,
-        MaterialPageRoute(builder: (context) => Profile(user: user)),
-      );
-    } catch (err) {
-      print(err.message);
-    }
-  }
+//   Future<void> signIn() async {
+//     try {
+//       FirebaseUser user = await FirebaseAuth.instance
+//           .signInWithEmailAndPassword(email: email, password: password);
+//       Navigator.push(
+//         context,
+//         MaterialPageRoute(builder: (context) => Profile(user: user)),
+//       );
+//     } catch (err) {
+//       print(err.message);
+//     }
+//   }
 
-  Widget emailFeild() {
-    return TextFormField(
-      keyboardType: TextInputType.emailAddress,
-      decoration: InputDecoration(
-        labelText: 'Email Adress',
-        hintText: 'you@example.com',
-      ),
-      validator: (String value) {
-        if (!value.contains('@')) {
-          return 'Enter a valie email';
-        }
-      },
-      onSaved: (String value) {
-        setState(() {
-          email = value;
-        });
-      },
-    );
-  }
+//   Widget emailFeild() {
+//     return TextFormField(
+//       keyboardType: TextInputType.emailAddress,
+//       decoration: InputDecoration(
+//         labelText: 'Email Adress',
+//         hintText: 'you@example.com',
+//       ),
+//       validator: (String value) {
+//         if (!value.contains('@')) {
+//           return 'Enter a valie email';
+//         }
+//       },
+//       onSaved: (String value) {
+//         setState(() {
+//           email = value;
+//         });
+//       },
+//     );
+//   }
 
-  Widget passwordFeild() {
-    return TextFormField(
-      keyboardType: TextInputType.emailAddress,
-      decoration: InputDecoration(
-        labelText: 'Password',
-        hintText: 'Password',
-      ),
-      obscureText: true,
-      validator: (String value) {
-        if (value.length < 4) {
-          return 'Password must be at least 4 letters long';
-        }
-      },
-      onSaved: (String value) {
-        setState(() {
-          password = value;
-        });
-      },
-    );
-  }
+//   Widget passwordFeild() {
+//     return TextFormField(
+//       keyboardType: TextInputType.emailAddress,
+//       decoration: InputDecoration(
+//         labelText: 'Password',
+//         hintText: 'Password',
+//       ),
+//       obscureText: true,
+//       validator: (String value) {
+//         if (value.length < 4) {
+//           return 'Password must be at least 4 letters long';
+//         }
+//       },
+//       onSaved: (String value) {
+//         setState(() {
+//           password = value;
+//         });
+//       },
+//     );
+//   }
 
-  Widget submitButton() {
-    return Padding(
-      padding: EdgeInsets.symmetric(vertical: 16.0),
-      child: RaisedButton(
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(10),
-        ),
-        onPressed: () {
-          if (formKey.currentState.validate()) {
-            formKey.currentState.save();
-            signIn();
-          }
-        },
-        padding: EdgeInsets.all(12),
-        color: Colors.pinkAccent[700],
-        child: Text('Submit', style: TextStyle(color: Colors.white)),
-      ),
-    );
-  }
-}
+//   Widget submitButton() {
+//     return Padding(
+//       padding: EdgeInsets.symmetric(vertical: 16.0),
+//       child: RaisedButton(
+//         shape: RoundedRectangleBorder(
+//           borderRadius: BorderRadius.circular(10),
+//         ),
+//         onPressed: () {
+//           if (formKey.currentState.validate()) {
+//             formKey.currentState.save();
+//             signIn();
+//           }
+//         },
+//         padding: EdgeInsets.all(12),
+//         color: Colors.pinkAccent[700],
+//         child: Text('Submit', style: TextStyle(color: Colors.white)),
+//       ),
+//     );
+//   }
+// }
