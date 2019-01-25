21285a90bf584a97dc6b1fcb713f31dd1c379e5a
Besslan <besslan.bazadough@gmail.com>
2019-01-25 23:41:04 +0200

    save shared prefernces with signin , rename login page

--- lib/src/login.dart
+++ lib/src/login.dart
@@ -1,123 +1,111 @@
-// import 'package:flutter/material.dart';
-// import 'package:firebase_auth/firebase_auth.dart';
-// import 'profile.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'profile.dart';
+import './blocs/bloc.dart';
 
-// class LoginPage extends StatefulWidget {
-//   @override
-//   LoginPageState createState() => new LoginPageState();
-// }
+class BlocLogIn extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: ListView(
+        shrinkWrap: true,
+        padding: EdgeInsets.only(left: 24.0, right: 24.0),
+        children: <Widget>[
+          Hero(
+            tag: 'hero',
+            child: CircleAvatar(
+              backgroundColor: Colors.transparent,
+              radius: 48.0,
+              child: Image.asset('assets/small-Logo.png'),
+            ),
+          ),
+          emailFeild(),
+          passwordFeild(),
+          submitButton(),
+        ],
+      ),
+    );
+  }
 
-// class LoginPageState extends State<LoginPage> {
-//   final formKey = GlobalKey<FormState>();
+  Future<void> signIn(context, email, password) async {
+    try {
+      await FirebaseAuth.instance
+          .signInWithEmailAndPassword(email: email, password: password);
+      SharedPreferences prefs = await SharedPreferences.getInstance();
+      await prefs.setString('loggedin', 'true');
 
-//   String email;
-//   String password;
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => Profile()),
+      );
+    } catch (err) {
+      print(err.message);
+    }
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.setString('loggedin', 'true');
+    await prefs.setString('email', email);
+  }
 
-//   TextEditingController emailController = TextEditingController();
-//   TextEditingController passwordController = TextEditingController();
+  Widget emailFeild() {
+    return StreamBuilder(
+      stream: bloc.email,
+      builder: (context, snapshot) {
+        return TextField(
+          onChanged: bloc.changeEmail,
+          keyboardType: TextInputType.emailAddress,
+          decoration: InputDecoration(
+            labelText: 'Email Adress',
+            hintText: 'you@example.com',
+            errorText: snapshot.error,
+          ),
+        );
+      },
+    );
+  }
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return Center(
-//       child: ListView(
-//         shrinkWrap: true,
-//         padding: EdgeInsets.only(left: 24.0, right: 24.0),
-//         children: <Widget>[
-//           Hero(
-//             tag: 'hero',
-//             child: CircleAvatar(
-//               backgroundColor: Colors.transparent,
-//               radius: 48.0,
-//               child: Image.asset('assets/small-Logo.png'),
-//             ),
-//           ),
-//           Form(
-//             key: formKey,
-//             child: Column(
-//               crossAxisAlignment: CrossAxisAlignment.stretch,
-//               children: <Widget>[
-//                 emailFeild(),
-//                 passwordFeild(),
-//                 submitButton(),
-//               ],
-//             ),
-//           ),
-//         ],
-//       ),
-//     );
-//   }
+  Widget passwordFeild() {
+    return StreamBuilder(
+      stream: bloc.password,
+      builder: (context, snapshot) {
+        return TextField(
+          onChanged: bloc.changepassword,
+          keyboardType: TextInputType.emailAddress,
+          decoration: InputDecoration(
+            labelText: 'Password',
+            hintText: 'Password',
+            errorText: snapshot.error,
+          ),
+          obscureText: true,
+        );
+      },
+    );
+  }
 
-//   Future<void> signIn() async {
-//     try {
-//       FirebaseUser user = await FirebaseAuth.instance
-//           .signInWithEmailAndPassword(email: email, password: password);
-//       Navigator.push(
-//         context,
-//         MaterialPageRoute(builder: (context) => Profile(user: user)),
-//       );
-//     } catch (err) {
-//       print(err.message);
-//     }
-//   }
-
-//   Widget emailFeild() {
-//     return TextFormField(
-//       keyboardType: TextInputType.emailAddress,
-//       decoration: InputDecoration(
-//         labelText: 'Email Adress',
-//         hintText: 'you@example.com',
-//       ),
-//       validator: (String value) {
-//         if (!value.contains('@')) {
-//           return 'Enter a valie email';
-//         }
-//       },
-//       onSaved: (String value) {
-//         setState(() {
-//           email = value;
-//         });
-//       },
-//     );
-//   }
-
-//   Widget passwordFeild() {
-//     return TextFormField(
-//       keyboardType: TextInputType.emailAddress,
-//       decoration: InputDecoration(
-//         labelText: 'Password',
-//         hintText: 'Password',
-//       ),
-//       obscureText: true,
-//       validator: (String value) {
-//         if (value.length < 4) {
-//           return 'Password must be at least 4 letters long';
-//         }
-//       },
-//       onSaved: (String value) {
-//         setState(() {
-//           password = value;
-//         });
-//       },
-//     );
-//   }
-
-//   Widget submitButton() {
-//     return Padding(
-//       padding: EdgeInsets.symmetric(vertical: 16.0),
-//       child: RaisedButton(
-//         shape: RoundedRectangleBorder(
-//           borderRadius: BorderRadius.circular(10),
-//         ),
-//         onPressed: () {
-//           if (formKey.currentState.validate()) {
-//             formKey.currentState.save();
-//             signIn();
-//           }
-//         },
-//         padding: EdgeInsets.all(12),
-//         color: Colors.pinkAccent[700],
-//         child: Text('Submit', style: TextStyle(color: Colors.white)),
-//       ),
-//     );
-//   }
-// }
+  Widget submitButton() {
+    return StreamBuilder(
+      stream: bloc.submitValid,
+      builder: (context, snapshot) {
+        return Padding(
+          padding: EdgeInsets.symmetric(vertical: 16.0),
+          child: RaisedButton(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10),
+            ),
+            onPressed: snapshot.hasData
+                ? () {
+                    List values = bloc.getValues();
+                    String validEmail = values[0];
+                    String validPassword = values[1];
+                    signIn(context, validEmail, validPassword);
+                  }
+                : null,
+            padding: EdgeInsets.all(12),
+            color: Colors.pinkAccent[700],
+            child: Text('Submit', style: TextStyle(color: Colors.white)),
+          ),
+        );
+      },
+    );
+  }
+}
