d32c2b30a032f5d9e006a2b7da69b748d3e5bbd8
Besslan <besslan.bazadough@gmail.com>
2019-01-29 01:00:39 +0200

    add Image picker plugin

--- lib/src/profile.dart
+++ lib/src/profile.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:image_picker/image_picker.dart';
 import 'drawer.dart';
 import 'dart:async';
 import 'sharedPrefs.dart';
